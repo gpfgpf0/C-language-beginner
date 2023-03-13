052d1d7daf452b1a96716728e1cfab72e6a26fd6
gpfgpf0 <122188531+gpfgpf0@users.noreply.github.com>
2023-03-13 15:49:24 +0800

    Add files via upload

---
+++ STL.cpp
@@ -0,0 +1,46 @@
+//https://wyqz.top/p/870124582.html
+// #include<bits/stdc++.h>
+//定义在哪儿都行，使用在函数内部
+#include<stdio.h>
+#include<stdlib.h>
+//栈
+// #include<stack>
+// using namespace std;
+// stack<int> s;
+// s.push(a);//入栈
+// s.pop();//出栈，无返回值
+// s.top();//返回栈顶元素但不移除,栈空报错
+// s.empty();//判断是否空
+// s.size();//返回栈内元素个数
+
+
+//队列
+// #include<queue>
+// using namespace std;
+// queue<int> q;
+// q.push(a);//尾部添加元素a
+// q.pop();//删除队首
+// q.front();//返回队首元素
+// q.back();//返回队尾元素
+// q.size();//返回队列中元素个数
+// q.empty();//判断是否为空
+
+
+//双向链表
+// #include<list>
+// using namespace std;
+// list<int> lt(n,ele);//不填为空，创建大小为n，（元素均为ele）
+// list<int> lt(begin,end);//用[begin,end)区间的数赋值
+// lt.assign();//n,ele或begin,end赋值
+// lt.push_front(a);//
+// lt.push_back(a);//
+// lt.insert(pos,,);//(n),ele或begin,end
+// lt.pop_front();//
+// lt.pop_back();//
+// lt.erase(pos);//
+int main()
+{
+    int a;
+    
+    return 0;
+}
